0b607b121bbe1f64357911e047012307faee6e29
eleshinlomo <seun.olatunji2@gmail.com>
2025-08-27 21:07:34 -0400

    mmm

--- server.js
+++ server.js
@@ -80,6 +80,7 @@ app.use('/api/prototype', prototypeRoutes);
 
 
 // Start the server
-app.listen(PORT, () => {
-    console.log('Server running on ', `http://localhost:${PORT}`);
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log('Server running on port:', PORT);
 });
