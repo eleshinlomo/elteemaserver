6032e56124d9c5e5a59273279eeacb2aa0a41b30
eleshinlomo <seun.olatunji2@gmail.com>
2025-09-02 08:09:08 -0400

    modify nodemailer

--- controllers/emailSender.js
+++ controllers/emailSender.js
@@ -1,26 +1,17 @@
-import nodemailer from 'nodemailer';
-const SERVICE = process.env.SERVICE
-const SUPPORT_EMAIL = process.env.SUPPORT_EMAIL
-const SUPPORT_PASS = process.env.SUPPORT_PASS
+import nodemailer from 'nodemailer'
 
-console.log('SUPPORT EMAIL', SUPPORT_EMAIL)
-console.log('SUPPORT PASS', SUPPORT_PASS)
-
-export const sendEmail = async (recieverEmail, messageSubject, emailBody ) => {
-    return  await new Promise((resolve, reject) => {
+export const sendEmail = async (recieverEmail, messageSubject, emailBody) => {
+    return await new Promise((resolve, reject) => {
         const transporter = nodemailer.createTransport({
-               service: 'gmail',
-              host: 'smtp.gmail.com',
-              port: 465,
-              secure: true,
+            service: 'gmail', // Let nodemailer handle Gmail settings automatically
             auth: {
-                user: SUPPORT_EMAIL,
-                pass: SUPPORT_PASS
+                user: process.env.SUPPORT_EMAIL,
+                pass: process.env.SUPPORT_PASS
             },
         });
 
         const mailOptions = {
-            from: `${SERVICE} <support@elteema.com>`,
+            from: `Elteema <${process.env.SUPPORT_EMAIL}>`,
             to: recieverEmail,
             subject: messageSubject,
             html: emailBody,
@@ -28,16 +19,12 @@ export const sendEmail = async (recieverEmail, messageSubject, emailBody ) => {
   
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
+                console.log('Email error:', error);
                 reject({ error: error.message, ok: false });  
             } else {
+                console.log('Email sent:', info.response);
                 resolve({ accepted: info.accepted, message: 'Email sent successfully!', ok: true }); 
             }
         });
-        
     });
-
-
 };
-
-
-
